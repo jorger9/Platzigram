f9c8684002ef3781fd364948eb250928c1aef729
Jorge Alberto Gómez Mendoza <jagomezmendoza@gmail.com>
2017-07-08 10:54:22 -0500

    FirebaseStorage

--- app/src/main/java/com/jorger9/platzigram/PlatzigramApplication.java
+++ app/src/main/java/com/jorger9/platzigram/PlatzigramApplication.java
@@ -1,6 +1,13 @@
 package com.jorger9.platzigram;
 
 import android.app.Application;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 /**
  * Created by jorger9 on 6/30/17.
@@ -8,8 +15,36 @@ import android.app.Application;
 
 public class PlatzigramApplication extends Application {
 
+    private static final String TAG = "PlatzigramApplication";
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseStorage firebaseStorage;
+
     @Override
     public void onCreate() {
+
         super.onCreate();
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+
+                if(firebaseUser != null){
+                    Log.w(TAG,"Usuario logueado" + firebaseUser.getEmail());
+                }
+                else
+                {
+                    Log.w(TAG,"Usuario no logueado");
+                }
+            }
+        };
+
+        firebaseStorage = FirebaseStorage.getInstance();
+    }
+
+    public StorageReference getStorageReference(){
+        return firebaseStorage.getReference();
     }
 }
--- app/src/main/java/com/jorger9/platzigram/post/view/NewPostActivity.java
+++ app/src/main/java/com/jorger9/platzigram/post/view/NewPostActivity.java
@@ -1,27 +1,92 @@
 package com.jorger9.platzigram.post.view;
 
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.jorger9.platzigram.PlatzigramApplication;
 import com.jorger9.platzigram.R;
 import com.squareup.picasso.Picasso;
 
+import java.io.ByteArrayOutputStream;
+
 public class NewPostActivity extends AppCompatActivity {
 
+    private static final String TAG = "NewPostActivity" ;
     private ImageView imgPhoto;
+    private Button btnCreatePost;
+    private String photoPath;
+    private PlatzigramApplication app;
+    private StorageReference storageReference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_post);
 
+        app = (PlatzigramApplication) getApplicationContext();
+        storageReference = app.getStorageReference();
+
         imgPhoto = (ImageView) findViewById(R.id.imgPhoto);
+        btnCreatePost = (Button) findViewById(R.id.btnCreatePost);
 
         if(getIntent().getExtras() != null){
+            photoPath = getIntent().getExtras().getString("PHOTO_PATH_TEMP");
+            showPhoto();
 
-            String photoPath = getIntent().getExtras().getString("PHOTO_PATH_TEMP");
-            Picasso.with(this).load(photoPath).into(imgPhoto);
         }
 
+        btnCreatePost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                upLoadPhoto();
+            }
+        });
+
+    }
+
+    private void upLoadPhoto() {
+        imgPhoto.setDrawingCacheEnabled(true);
+        imgPhoto.buildDrawingCache();
+
+        Bitmap bitmap = imgPhoto.getDrawingCache();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG,100,baos);
+
+        byte[] photoByte = baos.toByteArray();
+
+        String photoName = photoPath.substring(photoPath.lastIndexOf('/')+1,photoPath.length());
+        StorageReference photoReference = storageReference.child("postImages").child(photoName);
+
+        UploadTask uploadTask = photoReference.putBytes(photoByte);
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.w(TAG,"Errior al subir la foto" + e.toString() );
+                e.printStackTrace();
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Uri uriPhoto = taskSnapshot.getDownloadUrl();
+                String photoURL = uriPhoto.toString();
+                Log.w(TAG,"URL photo : " +photoURL);
+            }
+        });
+
+    }
+
+    private void showPhoto(){
+        Picasso.with(this).load(photoPath).into(imgPhoto);
     }
 }
